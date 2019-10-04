ff0254ea24b36c653353e1e9d04bfc2eb791fa77
Ritesh Kumar <riteshkumar@Saviynts-MacBook-Pro.local>
2019-10-04 16:27:18 +0530

    First commits

---
+++ src/components/Add.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Add extends Component {
+  render() {
+    return (
+      <div className="App">
+       <h1>Add Function for react jest application</h1>
+      </div>
+    );
+  }
+}
+
+export default Add;
---
+++ test/demo.test.js
@@ -0,0 +1,4 @@
+it("adds correctly", () => {
+    expect(1+1).toEqual(2);
+    // expect(1+1).toEqual(3);
+ });
